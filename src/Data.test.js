82eb560094164f796285e5f98eed7e0dd84134c7
Heman Robinson <heman.robinson@gmail.com>
2023-01-04 09:57:22 -0500

    update data test

--- src/Data.test.js
+++ src/Data.test.js
@@ -10,6 +10,7 @@ it( "returns column names", () => {
     expect( Data.getColumnNames( "Normal" )).toEqual([ "Random Normal, Bimodal" ]);
     expect( Data.getColumnNames( "Penguins" )).toEqual([ "Species", "Island", "Bill Length (mm)", "Bill Depth (mm)", "Flipper Length (mm)", "Body Mass (g)", "Sex" ]);
     expect( Data.getColumnNames( "Sales" )).toEqual([ "Model", "Cars Sold, 2022 Q1" ]);
+    expect( Data.getColumnNames( "Stocks" )).toEqual([ "Year", "S&P 500 Index" ]);
     expect( Data.getColumnNames( "Trends" )).toEqual([ "Week Number", "Week", "Google Trends 2020", "Interest" ]);
 });
 
@@ -19,6 +20,6 @@ it( "returns values", () => {
     expect( Data.getValues( "Normal" ).length ).toBe( 500 );
     expect( Data.getValues( "Penguins" ).length ).toBe( 342 );
     expect( Data.getValues( "Sales" ).length ).toBe( 288 );
+    expect( Data.getValues( "Stocks" ).length ).toBe( 153 );
     expect( Data.getValues( "Trends" ).length ).toBe( 1113 );
 });
-
