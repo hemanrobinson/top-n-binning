b7fd139ac86a2c0b78736b2a3374ccd0eca46d17
Heman Robinson <heman.robinson@gmail.com>
2023-03-22 15:22:37 -0400

    improve layout

--- src/App.js
+++ src/App.js
@@ -77,7 +77,6 @@ const App = () => {
                 <p>
                 The minimum bin width is the smallest visible width.  The maximum bin width puts all the data in one bin.  These are natural limits that users expect.
                 </p>
-                <br/>
                 <h2>Further Reading</h2>
                 <ul>
                     <li>Freedman, D. and Diaconis, P. (1981). "On the histogram as a density estimator: L2 theory". Zeitschrift für Wahrscheinlichkeitstheorie und Verwandte Gebiete. 57 (4): 453–476. <a href="https://bayes.wustl.edu/Manual/FreedmanDiaconis1_1981.pdf">https://bayes.wustl.edu/Manual/FreedmanDiaconis1_1981.pdf</a>.</li><br/>
